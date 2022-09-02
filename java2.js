dcb2f6959354671f7313d332cdf2b3729a6d9571
PythonCedric <osmanhulusii94@gmail.com>
2022-09-02 22:54:31 +0300

    javascript2
    javascript2

---
+++ java2.js
@@ -0,0 +1,15 @@
+let sonuc = 3;
+
+switch(sonuc){
+    case 1:
+        alert("sonucumuz 1 dir");
+        break;
+    case 2:
+        alert("sonuç 2 dir");
+        break;
+    case 3:
+        alert("sonuç 3 tür");
+        break;
+    default:
+        alert("sanırım tahnim edemedim")
+}
